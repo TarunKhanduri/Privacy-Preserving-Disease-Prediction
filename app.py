3ab4687b83168465929959676ec2bb01c272676d
Tarun Khanduri <91789442+TarunKhanduri@users.noreply.github.com>
2024-04-17 12:50:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request,url_for
+import logic
+app= Flask(__name__)
+
+# home page
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+# move to heart disease page or diabetes page
+@app.route('/disease', methods=["GET", "POST"])
+def disease():
+    if request.method == "POST":
+        page="index.html"
+        if request.form['text']=="heart":
+            page="heart"+".html"
+    return render_template(page)
+
+# heart disease analysis
+@app.route('/analyse_h', methods=["GET", "POST"])
+def analyse_h():
+    if request.method == "POST":
+        input=[]
+        for i in range(1,14):
+            input.append(float(request.form["input"+str(i)]))
+        m="Models/heart_model.pkl"
+        n="Models/heart_scaler.pkl"
+        
+        try:
+            output_svm = logic.predict_heart(m, n, input)
+        except Exception as e:
+            output_svm = str(e)
+        
+        try:
+            output_ckks = logic.predict_heart_fhe(m, n, input)
+        except Exception as e:
+            output_ckks = str(e)
+        
+        try:
+            output_ckks_dp = logic.predict_heart_fhe_dp(m, n, input)
+        except Exception as e:
+            output_ckks_dp = str(e)
+        
+        try:
+            output_paillier = logic.predict_heart_phe(m, n, input)
+        except Exception as e:
+            output_paillier = str(e)
+        
+        try:
+            output_dp = logic.predict_heart_dp(m, n, input)
+        except Exception as e:
+            output_dp = str(e)
+
+
+        return render_template("predict.html",opt1= output_svm, opt2=output_ckks, opt3= output_ckks_dp, opt4= output_paillier, opt5= output_dp)
+    return render_template("index.html")
+
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
+@app.route("/back", methods=["GET", "POST"])
+def back():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ logic.py
@@ -0,0 +1,197 @@
+########################################################
+#libraries
+import pickle
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import tenseal as ts
+import phe as paillier
+
+########################################################
+# loading model
+def model_scaler_heart(m,n):
+    with open(m,'rb') as f:
+        model_heart=pickle.load(f)
+    with open(n, 'rb') as f:
+        scaler_heart=pickle.load(f)
+    return model_heart,scaler_heart
+
+########################################################
+# FHE
+def predict_heart_fhe(m,n,input):
+    # key generation
+    context = ts.context(ts.SCHEME_TYPE.CKKS, poly_modulus_degree = 8192, coeff_mod_bit_sizes = [60, 40, 40, 60])
+    context.generate_galois_keys()
+    context.global_scale = 2**40
+
+    # model
+    model,scaler=model_scaler_heart(m,n)
+
+    # input
+    numpy_input = np.asarray(input)
+
+    # encryption
+    enc_input = ts.ckks_vector(context, numpy_input)
+
+    # coeff,intercept
+    coef = model.coef_.flatten()
+    coef = coef.reshape(-1)
+    enc_coef= ts.ckks_vector(context, coef)
+
+    enc_in=ts.ckks_vector(context, model.intercept_)
+
+    # prediction
+    prediction=enc_input.dot(enc_coef)
+    prediction += enc_in
+
+    # decryption
+
+    if(prediction.decrypt()[0] >= 0):
+        return 'The person have heart  disease'
+    else:
+        return 'The person does not have heart disease'
+
+########################################################
+# SHE
+
+
+########################################################
+# PHE
+def predict_heart_phe(m,n,input):
+    # key generation
+    public_key, private_key = paillier.generate_paillier_keypair()
+
+    # model
+    model,scaler=model_scaler_heart(m,n)
+
+
+    # encryption
+    enc_input=[]
+    for i in input:
+        enc_input.append(public_key.encrypt(i))
+
+    # input
+    numpy_input = np.asarray(enc_input)
+    
+    # coeff,intercept
+    coef = model.coef_.flatten()
+    coef = coef.reshape(-1)
+
+    inter = model.intercept_[0]
+
+    # prediction
+    prediction=0
+    for i in range(0,len(coef)):
+        prediction+=(enc_input[i] * coef[i])
+    prediction+= inter
+
+    # decryption
+
+    if(private_key.decrypt(prediction) >= 0):
+        return 'The person have heart  disease'
+    else:
+        return 'The person does not have heart disease'
+
+
+########################################################
+# DP 
+
+def add_laplace_noise(data, epsilon, reduction_factor):
+    sensitivity = 1 
+    original_scale = sensitivity / epsilon
+    reduced_scale = original_scale * reduction_factor
+    
+    noisy_data = []
+    for element in data:
+        noisy_data.append(element + np.random.laplace(scale=reduced_scale))
+
+    return noisy_data
+
+def predict_heart_dp(m,n,input):
+
+    # model
+    model,scaler=model_scaler_heart(m,n)
+
+    # input
+    numpy_input = np.asarray(input)
+
+    # Add noise in the input
+    noisy_input = add_laplace_noise(numpy_input, 1, 0.5)
+    noisy_input = np.array(noisy_input)
+
+    # coeff,intercept
+    coef = model.coef_.flatten()
+    coef = coef.reshape(-1)
+
+    inter = model.intercept_[0]
+
+    # prediction
+    prediction=0
+    for i in range(0,len(coef)):
+        prediction+=(noisy_input[i] * coef[i])
+    prediction+= inter
+
+    # decryption
+
+    if(prediction >= 0):
+        return 'The person have heart  disease'
+    else:
+        return 'The person does not have heart disease'
+
+########################################################
+# FHE with DP 
+
+def predict_heart_fhe_dp(m,n,input):
+
+    # key generation
+    context = ts.context(ts.SCHEME_TYPE.CKKS, poly_modulus_degree = 8192, coeff_mod_bit_sizes = [60, 40, 40, 60])
+    context.generate_galois_keys()
+    context.global_scale = 2**40
+
+    # model
+    model,scaler=model_scaler_heart(m,n)
+
+    # input
+    numpy_input = np.asarray(input)
+
+    # Add noise in the input
+    noisy_input = add_laplace_noise(numpy_input, 1, 0.5)
+    noisy_input = np.array(noisy_input)
+
+    # encryption
+    enc_input = ts.ckks_vector(context, noisy_input)
+
+    # coeff,intercept
+    coef = model.coef_.flatten()
+    coef = coef.reshape(-1)
+    enc_coef= ts.ckks_vector(context, coef)
+
+    enc_in=ts.ckks_vector(context, model.intercept_)
+
+    # prediction
+    prediction=enc_input.dot(enc_coef)
+    prediction += enc_in
+
+    # decryption
+
+    if(prediction.decrypt()[0] >= 0):
+        return 'The person have heart  disease'
+    else:
+        return 'The person does not have heart disease'
+
+########################################################
+# prediction without any encryption
+def predict_heart(m,n,input):
+    model,scaler=model_scaler_heart(m,n)
+
+    numpy_input = np.asarray(input)
+
+    # reshape array for predicting 1 instance
+    input_reshaped = numpy_input.reshape(1,-1)
+
+    std_data = scaler.transform(input_reshaped)
+    prediction = model.predict(std_data)
+
+    if(prediction[0] == 0):
+        return 'The person does not have heart  disease'
+    else:
+        return 'The person has disease'
